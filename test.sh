4096a1eafb8e3290430355829dc5d142cc387ec0
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2013-12-25 21:58:16 +0100

    Better test runner

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 for f in test/*; do
-  echo $f
   class_name=`echo $f | sed 's/test\/\([a-z]*\)_test.ck/\1/g'`
+
+  echo ""
+  echo -e "\e[1mTesting: $class_name\e[21m"
   chuck lib/$class_name $f;
 done
